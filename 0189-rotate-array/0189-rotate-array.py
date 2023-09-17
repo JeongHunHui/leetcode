064a6723ddf5aa7fe5da5874b99d66b2a9b3295b
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-09-18 01:18:34 +0900

    Time: 175 ms (86.15%), Space: 27.8 MB (65.92%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,9 +1,5 @@
-from collections import deque
-
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        new_nums = deque(nums)
-        for _ in range(k):
-            new_nums.appendleft(new_nums.pop())
-        nums[:] = list(new_nums)
+        n = len(nums) - k % len(nums)
+        nums[:] = nums[n:] + nums[:n]
         
