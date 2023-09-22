a42be4d9bb972bf02c26b19eb701f9dcd87e1be4
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-09-22 17:57:41 +0900

    Time: 48 ms (40.94%), Space: 16.5 MB (96.79%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,10 +1,8 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        answer = 0
+        # h번 인용된 논문이 h개 이상일 때 h의 최댓값 구하기
         citations.sort(reverse=True)
-        for c in citations:
-            if c > answer:
-                answer += 1
-            else:
-                break
-        return answer
+        for i in range(len(citations)):
+            if citations[i] < i+1:
+                return i
+        return len(citations)
