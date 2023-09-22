764f95f3b3ec1914e9d573de0ef13a6fbcec46c6
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-09-22 18:14:29 +0900

    Time: 492 ms (29.67%), Space: 63.5 MB (90.19%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -2,19 +2,19 @@ from random import sample
 
 class RandomizedSet:
     def __init__(self):
-        self.rand_set = set()
+        self.hash_map = {}
 
     def insert(self, val: int) -> bool:
-        if val in self.rand_set:
+        if val in self.hash_map:
             return False
-        self.rand_set.add(val)
+        self.hash_map[val] = 0
         return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.rand_set:
+        if val not in self.hash_map:
             return False
-        self.rand_set.remove(val)
+        self.hash_map.pop(val)
         return True
 
     def getRandom(self) -> int:
-        return sample(self.rand_set, 1)[0]
+        return random.choice(list(self.hash_map.keys()))
