858429f9a7104509cb39c46fb4c0afb06489d601
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-09-23 21:57:21 +0900

    Time: 61 ms (16.45%), Space: 16 MB (99.96%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_dict = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+        answer = 0
+        i = 0
+        while i < len(s):
+            c = s[i:i+2]
+            if c in roman_dict:
+                answer += roman_dict[c]
+                i += 2
+            else:
+                answer += roman_dict[c[0]]
+                i += 1
+        return answer
