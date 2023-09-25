98e813b5352d19b9470cbb8382a78a5b272063f9
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-09-26 01:50:19 +0900

    Time: 58 ms (31.24%), Space: 16.2 MB (78.08%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,14 +1,14 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         roman_datas = [('M',1000),('CM',900),('D',500),('CD',400),('C',100),('XC',90),('L',50),('XL',40),('X',10),('IX',9),('V',5),('IV',4),('I',1)]
-        c_roman, c_num = roman_datas[0]
         point = 0
         answer = ''
+        c_roman, c_num = roman_datas[0]
         while num > 0:
-            if c_num > num:
+            if num < c_num:
                 point += 1
                 c_roman, c_num = roman_datas[point]
             else:
-                num -= c_num
                 answer += c_roman
+                num -= c_num
         return answer
