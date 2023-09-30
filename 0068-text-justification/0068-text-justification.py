3abca48457588d107fe74eb5423cd231cc60a79c
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-09-30 16:41:51 +0900

    Time: 36 ms (77.61%), Space: 16.4 MB (61.6%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -1,39 +1,28 @@
-import math
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
         arr = []
         s = words[0]
-        for i in range(1, len(words)):
-            word = words[i]
-            print(s, i,word)
-            l = len(s)
-            cl = len(word)
-            print(l + cl + 1 <= maxWidth)
-            if l + cl + 1 <= maxWidth:
+        for word in words[1:]:
+            if len(s) + len(word) + 1 <= maxWidth:
                 s += ' ' + word
             else:
                 s_arr = s.split()
-                # 공백 수
                 blank = len(s_arr) - 1
-                # 필요한 전체 공백 수
-                blank_count = maxWidth - l + blank
+                blank_count = maxWidth - len(s) + blank
                 if blank == 0:
                     arr.append(s+' '*blank_count)
                     s = word
                     continue
-                # 한 공백당 칸 수
-                blank_avg = blank_count // blank
-                blank_left = blank_count % blank
-                answer = ''
+                blank_avg, blank_left = divmod(blank_count, blank)
                 if blank_left == 0:
-                    answer = (' '*blank_avg).join(s_arr)
+                    arr.append((' '*blank_avg).join(s_arr))
                 else:
+                    answer = ''
                     for i in range(blank_left):
                         answer += s_arr[i] + ' '*(blank_avg+1)
                     for i in range(blank_left, blank):
                         answer += s_arr[i] + ' '*blank_avg
-                    answer += s_arr[-1]
-                arr.append(answer)
+                    arr.append(answer + s_arr[-1])
                 s = word
         arr.append(s+' '*(maxWidth-len(s)))
         return arr
